ea6d6807b7e40c39d809d1504ab20addb17526c0
BT <72067154+beketamas@users.noreply.github.com>
2025-04-04 08:36:43 +0200

    add wpf

---
+++ VIZSGA_KOROM_WPF/VIZSGA_KOROM_WPF/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VIZSGA_KOROM.Models;
+
+namespace VIZSGA_KOROM_WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        List<Auto> autok = [];
+        btnBetolt.Click += (s, e) => 
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    autok = Auto.ReadCSV(openFileDialog.FileName);
+                    dgAutok.ItemsSource = autok;
+
+                }
+                catch (Exception n)
+                {
+
+                    MessageBox.Show($"Hiba: {n}", "!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        };
+        tbEv.TextChanged += (s, e) =>
+        {
+            if (s is TextBox mezo)
+                lbAutok.ItemsSource = autok.Where(x => x.GyartasiEv == mezo.Text).Select(y => $"{y.Marka} {y.Model}").ToList();
+        };
+        btnBezar.Click += (s, e) =>
+        {
+            var valasz = MessageBox.Show("Bizots ki akar lépni?", "?" , MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (valasz == MessageBoxResult.Yes)
+                Close();
+
+            return;
+        };
+    }
+}
---
+++ VIZSGA_KOROM_WPF/VIZSGA_KOROM_WPF/Models/Auto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIZSGA_KOROM.Models
+{
+    public class Auto
+    {
+        string sorszam;
+        string marka;
+        string model;
+        string gyartasiEv;
+        string feher;
+        int eladottDarabSzam;
+        double atlagosEladasiAr;
+
+        public Auto(string[] tomb)
+        {
+            this.sorszam = tomb[0];
+            this.marka = tomb[1];
+            this.model = tomb[2];
+            this.gyartasiEv = tomb[3];
+            this.feher = tomb[4];
+            this.eladottDarabSzam = int.Parse(tomb[5]);
+            this.atlagosEladasiAr = double.Parse(tomb[6]);
+        }
+
+
+        public string Sorszam { get => sorszam; set => sorszam = value; }
+        public string Marka { get => marka; set => marka = value; }
+        public string Model { get => model; set => model = value; }
+        public string GyartasiEv { get => gyartasiEv; set => gyartasiEv = value; }
+        public string Feher { get => feher; set => feher = value; }
+        public int EladottDarabSzam { get => eladottDarabSzam; set => eladottDarabSzam = value; }
+        public double AtlagosEladasiAr { get => atlagosEladasiAr; set => atlagosEladasiAr = value; }
+
+        public static List<Auto> ReadCSV(string nev) => [.. File.ReadLines("autok.csv").Skip(1).Select(x => new Auto(x.Split(";")))];
+    }
+}
